e6ae6a6112e7f8b6cc928587a744fb8a0073a4f0
Gustavo Martins Lopes <gstvmlopes25@gmail.com>
2023-10-09 13:07:13 -0300

    codigo js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
-function login(){
-    if( email.value == "teste@teste.com" && senha.value == "senha"){
+const form = document.querySelector('form');
+    
+form.addEventListener('submit', event =>{
+    event.preventDefault();
+    const email = document.querySelector('#email');
+    console.log(email.value);
+    const senha = document.querySelector('#senha');
+    console.log(senha.value);
+
+    if(email.value === 'a@a' & senha.value === 'aaa'){
         window.location.href = "./../pages/dashboard.html";
     }
-}
+})
+
